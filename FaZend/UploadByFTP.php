31dc65448a345420826e8ad9ec1225e43d256673
Yegor Bugayenko <yegor@tpc2.com>
2009-05-09 08:26:52 +0000

    uploader added
    git-svn-id: http://fazend.googlecode.com/svn/trunk@8 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/UploadByFTP.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+require_once 'phing/Task.php';
+
+/**
+* This is Phing Task for uploading all files to FTP
+*
+* @see http://phing.info/docs/guide/current/chapters/ExtendingPhing.html#WritingTasks
+*/
+class UploadByFTP extends Task {
+
+	// these directories/files won't be uploaded
+	private static $forbidden = array (
+		'.svn',
+	);
+
+	private $server;
+	private $userName;
+	private $password;
+	private $destDir;
+	private $srcDir;
+
+	/**
+	* Initiator (when the build.xml sees the task)
+	* 
+	* @return void
+	*/
+	public function init () {
+	}
+
+	/**
+	* Executes
+	* 
+	* @return void
+	*/
+	public function main () {
+		$this->Log ("FTP params received\n\tserver: {$this->server}\n\tlogin: {$this->userName}\n\tpassword: ".
+			preg_replace ('/./', '*', $this->password)."\n\tsrcDir: '{$this->srcDir}'\n\tdestDir: '{$this->destDir}'");
+
+		$this->ftp = @ftp_connect ($this->server);
+		if (!$this->ftp)
+			throw new BuildException ("Failed to connect to ftp ({$this->server})");	
+
+		$this->Log ("Logged in successfully to {$this->server}");	
+
+		if (!@ftp_login ($this->ftp, $this->userName, $this->password))
+			throw new BuildException ("Failed to login to ftp ({$this->server})");	
+
+		$this->Log ("Connected successfully to FTP as {$this->userName}");	
+
+		if (!@ftp_pasv ($this->ftp, true))
+			throw new BuildException ("Failed to turn PASV mode ON");	
+
+		if (!@ftp_chdir ($this->ftp, $this->destDir))
+			throw new BuildException ("Failed to go to {$this->destDir}");	
+
+		$this->Log ("Current directory in FTP: ".ftp_pwd ($this->ftp));	
+
+		$this->filesUploaded = 0;
+		$this->uploadFiles ($this->srcDir);
+		$this->Log ("Uploaded {$this->filesUploaded} files");	
+
+		if (!@ftp_close ($this->ftp))
+			throw new BuildException ("Failed to close connection to ftp ({$this->server})");	
+
+		$this->Log ("Disconnected from FTP");	
+	}
+
+	/**
+	* Upload files, recursively
+	*
+	* @param string
+	* @return void
+	*/
+	public function uploadFiles ($path) {
+
+		$dir = scandir ($path);
+		
+		$ftpList = @ftp_nlist ($this->ftp, '.');	
+		if (!$ftpList)
+			throw new BuildException ("Failed to get nlist from FTP: '$entry'");	
+
+		// delete obsolete elements from FTP server	
+		foreach ($ftpList as $ftpEntry) {
+			if (!in_array ($ftpEntry, $dir)) {
+				if (@ftp_delete ($this->ftp, $ftpEntry))
+					continue;
+
+				// maybe it's a directory?
+				// I can't delete directories recursively yet...
+				//throw new BuildException ("Failed to delete FTP file '$ftpEntry' in ".ftp_pwd ($this->ftp));	
+			}	
+		}	
+
+		foreach ($dir as $entry) {
+
+			if (($entry == '.') || ($entry == '..') || in_array ($entry, self::$forbidden))
+				continue;
+
+			$fileName = $path.'/'.$entry;
+
+			if (is_dir ($fileName)) {
+				// this directory doesn't exist yet on the server, we should create it
+				if (!@ftp_chdir ($this->ftp, $entry)) {
+					if (!@ftp_mkdir ($this->ftp, $entry))
+						throw new BuildException ("Failed to create dir '$entry' in ".ftp_pwd ($this->ftp));	
+
+					if (!@ftp_chdir ($this->ftp, $entry))	
+						throw new BuildException ("Failed to chdir to '$entry' in ".ftp_pwd ($this->ftp));	
+	
+					$this->Log ("Created directory $entry");	
+				}
+
+				$this->uploadFiles ($fileName);
+
+				if (!@ftp_cdup ($this->ftp))	
+					throw new BuildException ("Failed to cdup from '$entry' in ".ftp_pwd ($this->ftp));	
+	
+			} else {
+
+				// this file already exists?
+				if (in_array ($entry, $ftpList)) {
+					$lastModified = @ftp_mdtm ($this->ftp, $entry);
+					if ($lastModified == -1)
+						throw new BuildException ("Failed to get mdtm from '$entry'");	
+
+					// if the server version is younger than the local - we skip this file	
+					if ($lastModified > filemtime ($fileName))
+						continue;	
+				}	
+
+				if (!@ftp_put ($this->ftp, $entry, $fileName, FTP_BINARY))	
+					throw new BuildException ("Failed to upload '$fileName' (".filesize ($fileName)." bytes)");	
+
+				$this->filesUploaded++;
+				$this->Log ("Uploaded $fileName (".filesize ($fileName)." bytes)");	
+			}	
+
+		}
+
+	}
+
+	/**
+	* Initalizer
+	*
+	* @param $fileName string
+	*/
+	public function setserver ($server) {
+		$this->server = $server;
+	}
+
+	/**
+	* Initalizer
+	*
+	* @param $fileName string
+	*/
+	public function setusername ($userName) {
+		$this->userName = $userName;
+	}
+
+	/**
+	* Initalizer
+	*
+	* @param $fileName string
+	*/
+	public function setpassword ($password) {
+		$this->password = $password;
+	}
+
+	/**
+	* Initalizer
+	*
+	* @param $fileName string
+	*/
+	public function setdestdir ($destDir) {
+		$this->destDir = $destDir;
+	}
+
+	/**
+	* Initalizer
+	*
+	* @param $fileName string
+	*/
+	public function setsrcdir ($srcDir) {
+		$this->srcDir = $srcDir;
+	}
+
+}
