adb487bf1fd6dba198c1689469abcaf6d6d5e4c1
Yegor Bugayenko <yegor@tpc2.com>
2009-05-14 06:48:51 +0000

    page load time introduced
    git-svn-id: http://fazend.googlecode.com/svn/trunk@62 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/View/Helper/PageLoadTime.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ *
+ * @see http://naneau.nl/2007/07/08/use-the-url-view-helper-please/
+ */
+class FaZend_View_Helper_PageLoadTime {
+
+	public function pageLoadTime() {
+ 		global $startTime; 
+ 		return sprintf("%0.2f sec", round(microtime(true)-$startTime, 2));
+ 	}
+
+}
