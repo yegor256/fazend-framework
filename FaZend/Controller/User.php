a2559dd3b16667137b1fd29826ac63b3d8743899
Yegor Bugayenko <yegor@tpc2.com>
2009-05-20 09:12:14 +0000

    User controller introduced
    git-svn-id: http://fazend.googlecode.com/svn/trunk@110 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/Controller/User.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * User management controller
+ *
+ *
+ */
+class FaZend_Controller_User extends FaZend_Controller_Action {
+
+        /**
+         * Register new account
+         *
+         * @return void
+         */
+        public function registerAction() {
+
+        	if (FaZend_User::isLoggedIn())
+        		return $this->_forwardWithMessage('you are already logged in');
+
+                $form = FaZend_Form::create('RegisterAccount', $this->view);
+	        if (!$form->isFilled())
+	        	return;
+
+		$email = $form->email->getValue();	
+		$password = $form->password->getValue();
+
+		try {
+			$user = FaZend_User::register($email, $password);
+		} catch (Model_User_RegisterException $e) {
+			$form->email->addError($e->getMessage());
+			return;
+		}	
+
+		Model_User::logIn($email, $password);
+
+		$this->_forward('index', 'index');
+
+        }
+        	
+        /**
+         * Remind password
+         *
+         * @return void
+         */
+        public function remindAction() {
+
+        	if (Model_User::isLoggedIn()) 
+        		return $this->_forwardWithMessage('you are already logged in');
+
+                $form = FaZend_Form::create('RemindPassword', $this->view);
+	        if (!$form->isFilled())
+	        	return;
+
+		try {
+			$user = FaZend_User::findByEmail($form->email->getValue());
+		} catch (Exception $e) {
+			$form->email->addError('user not found');
+			return;
+		}
+
+		// send password by email	
+		FaZend_Email::create('RemindPassword.tmpl')
+			->set('toEmail', $user->email)
+			->set('toName', $user->nickname)
+			->set('password', $user->password)
+			->send();
+
+		$this->_forward('reminded');	
+
+        }
+        	
+        /**
+         * Remind password - done
+         *
+         * @return void
+         */
+        public function remindedAction() {
+        }
+
+        /**
+         * Log out
+         *
+         * @return void
+         */
+        public function logoutAction() {
+
+        	if (!Model_User::isLoggedIn()) 
+        		return $this->_forwardWithMessage('you are not logged in yet');
+
+        	FaZend_User::logOut();
+
+        	$this->_forward('index', 'index');
+
+        }
+
+        /**
+         * Log in
+         *
+         * @return void
+         */
+        public function loginAction() {
+
+		$loginEmail = $this->getRequest()->getPost('loginEmail');
+		$loginPassword = $this->getRequest()->getPost('loginPassword');
+
+		if ($loginEmail && $loginPassword) {
+			try {
+				Model_User::logIn($loginEmail, $loginPassword);
+			} catch (FaZend_User_LoginException $e) {
+				$this->view->loginError = $e->getMessage();
+			}	
+		}	
+
+		$this->view->loginEmail = $loginEmail;
+		$this->view->loginPassword = $loginPassword;
+
+		$this->_forward('index', 'index');
+	}	
+
+}
