f0e85c976b7e7125c5e279829f208c70aa665977
Yegor Bugayenko <yegor@tpc2.com>
2009-06-04 16:05:04 +0000

    git-svn-id: http://fazend.googlecode.com/svn/trunk@204 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/Cli/OptionMissedException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * Option is missed
+ *
+ * @package FaZend 
+ */
+class FaZend_Cli_OptionMissedException extends Exception {
+}
