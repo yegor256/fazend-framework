62f6beaf6c41653afba30bd22ce5c4a8294919a5
Yegor Bugayenko <yegor@tpc2.com>
2009-06-25 07:29:15 +0000

    includeJS introduced
    git-svn-id: http://fazend.googlecode.com/svn/trunk@378 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/View/Helper/IncludeJS.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * Include single JS file
+ *
+ * @see http://naneau.nl/2007/07/08/use-the-url-view-helper-please/
+ * @package FaZend 
+ */
+class FaZend_View_Helper_IncludeJS extends FaZend_View_Helper {
+
+	/**
+	* Include a JS file as a link
+	*
+	* @return void
+	*/
+	public function includeJS($script) {
+
+		$this->getView()->headScript()->appendFile($this->getView()->url(array('script'=>$script), 'js', true));
+
+	}
+
+}
