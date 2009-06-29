27965d9c6f9e489ce9031b4fbc572343ad97b2db
Yegor Bugayenko <yegor@tpc2.com>
2009-06-29 17:50:19 +0000

    fix
    git-svn-id: http://fazend.googlecode.com/svn/trunk@444 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Paginator.php
+++ FaZend/Paginator.php
@@ -28,7 +28,7 @@ class FaZend_Paginator extends Zend_Paginator {
      */
     public static function addPaginator($iterator, Zend_View $view, $page, $name = 'paginator') {
         if ($iterator instanceof FaZend_Db_RowsetWrapper)
-            $adapter = new Zend_Paginator_Adapter_DbSelect($iterator->select());
+            $adapter = new Zend_Paginator_Adapter_DbTableSelect($iterator->select());
         else
             $adapter = new Zend_Paginator_Adapter_Iterator($iterator);
 
