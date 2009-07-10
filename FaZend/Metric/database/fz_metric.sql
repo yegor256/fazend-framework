a14ed1bfeed97cd68d2a9047c24b5cce0a730a6e
Alex Mahomet <a.mahomet@gmail.com>
2009-07-10 10:31:13 +0000

    environment field was deleted
    git-svn-id: http://fazend.googlecode.com/svn/trunk@509 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Metric/database/fz_metric.sql
+++ FaZend/Metric/database/fz_metric.sql
@@ -7,7 +7,6 @@ CREATE TABLE IF NOT EXISTS `fz_metric` (
     `method` VARBINARY(255) NOT NULL COMMENT "'class::method' combination of the calculation function",
     `params` LONGTEXT BINARY COMMENT "Serialized associative array of params to be passed",
     `value` LONGTEXT BINARY COMMENT "Result value of the function, serialized",
-    `environment` VARBINARY(32) NOT NULL COMMENT "Unique ID of the environment",
     `msec` INT UNSIGNED COMMENT "Time used to calculate the value",
     `updated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT "Date of this metric calculation",
     PRIMARY KEY (`id`),
