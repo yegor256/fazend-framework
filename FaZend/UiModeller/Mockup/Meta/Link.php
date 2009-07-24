3040ffa86c4fae12dcbd2c5dd942818d15a2cf7f
Yegor Bugayenko <yegor@tpc2.com>
2009-07-24 13:03:18 +0000

    other mockup elements
    git-svn-id: http://fazend.googlecode.com/svn/trunk@645 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/UiModeller/Mockup/Meta/Link.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * Mockup meta element, text
+ *
+ * @package FaZend 
+ */
+class FaZend_UiModeller_Mockup_Meta_Link extends FaZend_UiModeller_Mockup_Meta_Abstract {
+
+    /**
+     * Draw 
+     *
+     * @return int Height
+     */
+    public function draw($y) {
+
+
+    }
+
+}
