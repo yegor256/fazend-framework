517d693399b55e4d30eec15b62a0107072c2df54
Yegor Bugayenko <yegor@tpc2.com>
2009-07-30 13:14:52 +0000

    fix
    git-svn-id: http://fazend.googlecode.com/svn/trunk@724 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/View/Helper/HtmlTable.php
+++ FaZend/View/Helper/HtmlTable.php
@@ -274,7 +274,7 @@ class FaZend_View_Helper_HtmlTable extends FaZend_View_Helper {
 
                 // parse the value of this TD with helper
                 if ($this->_column($title)->helper) {
-                    $parser = $this->_column($title)->helper;
+                    $helper = $this->_column($title)->helper;
                     $value = $this->getView()->$helper($value, $rowOriginal);
                 }    
 
