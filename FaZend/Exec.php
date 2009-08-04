a88e9336bbf393344118f3f3f26a398a9a683d3f
Yegor Bugayenko <yegor@tpc2.com>
2009-08-04 11:46:12 +0000

    fix
    git-svn-id: http://fazend.googlecode.com/svn/trunk@745 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Exec.php
+++ FaZend/Exec.php
@@ -81,7 +81,7 @@ class FaZend_Exec extends FaZend_StdObject {
         if (!$this->isRunning())
             return false;
 
-        return time() - filemtime(self::_fileName($id, self::PID_SUFFIX));
+        return time() - @filemtime(self::_fileName($id, self::PID_SUFFIX));
 
     }
 
