e36a2a81308798379b51483d4d6a3076d5abdbf5
Yegor Bugayenko <yegor@tpc2.com>
2009-09-20 15:36:25 +0000

    validator test
    git-svn-id: http://fazend.googlecode.com/svn/trunk@870 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ test/FaZend/ValidatorTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+require_once 'AbstractTestCase.php';
+
+/**
+ * Test the validator mechanism
+ *
+ * @package test
+ */
+class FaZend_ValidatorTest extends AbstractTestCase {
+
+    /**
+     * Test it
+     *
+     * @return void
+     */
+    public function testSimpleScenarioWorks() {
+
+        // positive case
+        validate()
+            ->true(true, 'works')
+            ->false(false)
+            ->type('test', 'string')
+            ->regexp('test', '/^\w+$/')
+            ->instanceOf(new FaZend_StdObject, 'FaZend_StdObject');
+            
+        // negative case
+        try {
+            validate()
+                ->true(false, 'it is ok');
+            $this->fail('Exception should be raised');
+        } catch (FaZend_Validator_Failure $e) {
+            // it's fine
+        }
+
+    }
+
+}
