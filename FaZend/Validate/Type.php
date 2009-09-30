ae129cad3bb1213a2c39826f564016da57998a12
Yegor Bugayenko <yegor@tpc2.com>
2009-09-30 13:17:03 +0000

    TYPE validator
    git-svn-id: http://fazend.googlecode.com/svn/trunk@934 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/Validate/Type.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * Object is of some type
+ *
+ * @package FaZend
+ */
+class FaZend_Validate_Type extends Zend_Validate_Abstract {
+
+    const INVALID = 'invalid';
+    const WRONG_TYPE = 'wrongType';
+
+    /**
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::INVALID   => "Object is not of type '%type%'",
+        self::WRONG_TYPE   => "Type '%type%' is unknown for this validator",
+    );
+
+    /**
+     * @var array
+     */
+    protected $_messageVariables = array(
+        'type' => '_type'
+    );
+
+    /**
+     * Name of the type class/iface
+     *
+     * @var string
+     */
+    protected $_type;
+
+    /**
+     * Sets validator options
+     *
+     * @param  string Name of the type class/iface
+     * @return void
+     */
+    public function __construct($type) {
+        $this->settype($type);
+    }
+
+    /**
+     * Returns the type
+     *
+     * @return string
+     */
+    public function gettype() {
+        return $this->_type;
+    }
+
+    /**
+     * Sets the type
+     *
+     * @param  string The type
+     * @return Zend_Validate_Regex Provides a fluent interface
+     */
+    public function settype($type) {
+        $this->_type = (string) $type;
+        return $this;
+    }
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if and only if $value is instance of some class
+     *
+     * @param  string $value
+     * @return boolean
+     */
+    public function isValid($value) {
+        switch ($this->_type) {
+            
+            case 'string':
+            case 'str':
+                $ok = is_string($value);
+                break;
+            
+            case 'int':
+            case 'integer':
+                $ok = is_int($value);
+                break;
+                
+            case 'bool':
+            case 'boolean':
+                $ok = is_boolean($value);
+                break;
+        }
+        
+        if (!isset($ok)) {
+            $this->_error(self::WRONG_TYPE);
+            return false;
+        }
+                
+        if (!$ok) {
+            $this->_error(self::INVALID);
+            return false;
+        }
+        return true;
+    }
+
+}
