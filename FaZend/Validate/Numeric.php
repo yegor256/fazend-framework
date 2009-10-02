690f6078f47124ad224482a6aeeaec5f2b4aebf9
Yegor Bugayenko <yegor@tpc2.com>
2009-10-02 13:01:02 +0000

    numeric validator
    git-svn-id: http://fazend.googlecode.com/svn/trunk@958 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/Validate/Numeric.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * One simple validator for numeric values
+ *
+ * @package FaZend
+ */
+class FaZend_Validate_Numeric extends Zend_Validate_Abstract {
+
+    const INVALID = 'invalid';
+
+    /**
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::INVALID   => "NON-NUMERIC value given, instead of a number",
+    );
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if and only if $value is true
+     *
+     * @param  string $value
+     * @return boolean
+     */
+    public function isValid($value) {
+        if (!is_numeric($value)) {
+            $this->_error(self::INVALID);
+            return false;
+        }
+        return true;
+    }
+
+}
