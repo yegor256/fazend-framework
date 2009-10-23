69ce63ec318222f3145ec79d539ad16ed317f0b6
Yegor Bugayenko <yegor@tpc2.com>
2009-10-23 08:13:58 +0000

    FaZend_Date introduced
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1007 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/Date.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Copyright (c) FaZend.com
+ * All rights reserved.
+ *
+ * You can use this product "as is" without any warranties from authors.
+ * You can change the product only through Google Code repository
+ * at http://code.google.com/p/fazend
+ * If you have any questions about privacy, please email privacy@fazend.com
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+/**
+ * Zend_Date decorator
+ *
+ * @package Date
+ */
+class FaZend_Date {
+
+    /**
+     * Create Zend_Date object on-fly
+     *
+     * @return FaZend_Date
+     **/
+    public static function factory($time) {
+        return new FaZend_Date($time);
+    }
+
+}
