e71ad7dd76c3b377ec250efc80065fadbdc441a4
Yegor Bugayenko <yegor@tpc2.com>
2009-10-29 19:22:30 +0000

    tests reorganized
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1035 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- test/FaZend/Pan/Database/MapTest.php
+++ test/FaZend/Pan/Database/MapTest.php
@@ -16,11 +16,11 @@
 
 require_once 'AbstractTestCase.php';
 
-class FaZend_Deployer_MapTest extends AbstractTestCase {
+class FaZend_Pan_Database_MapTest extends AbstractTestCase {
 
     public function testPngBuilderWorks() {
 
-        $map = new FaZend_Deployer_Map();
+        $map = new FaZend_Pan_Database_Map();
 
         $this->assertNotEquals(false, $map->png());
 
