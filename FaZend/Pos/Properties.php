bf97108618401d3d8f1cd8d032beeb9d920efe29
Yegor Bugayenko <yegor@tpc2.com>
2009-12-22 08:24:09 +0000

    typo in POS
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1271 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Pos/Properties.php
+++ FaZend/Pos/Properties.php
@@ -494,7 +494,7 @@ class FaZend_Pos_Properties
     {
         $class = $fzObject->class;
         $obj = new $class();
-        $obj->_setMyParent($this);
+        $obj->ps()->_setMyParent($this);
         return $obj;
     }
 
