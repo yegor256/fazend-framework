05ba9be510cee66eb0d002a6cb406b51b8895ec2
Yegor Bugayenko <yegor@tpc2.com>
2010-01-28 12:58:07 +0000

    killed obsolete files
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1549 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Cache/Backend/cacheRow.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `__cacherow` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "Unique ID of the data row",
-	`label` VARBINARY(1024) NOT NULL COMMENT "Label of the data",
-	`data` LARGETEXT NOT NULL COMMENT "Value of data",
-	`created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT "When this row was created",
-	`lifetime` INT NOT NULL COMMENT "Time in seconds to live (FALSE = live forever)",
-	PRIMARY KEY USING BTREE (`id`),
-	INDEX (`label`)
-	) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ENGINE=Memory;
