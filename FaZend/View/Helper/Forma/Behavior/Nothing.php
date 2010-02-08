0ee09b75209c63dd1e20abe257d69d2e1915ed25
Yegor Bugayenko <yegor@tpc2.com>
2010-02-08 14:15:11 +0000

    Nothing behavior introduced for forma()
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1609 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/View/Helper/Forma/Behavior/Nothing.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * FaZend Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt. It is also available 
+ * through the world-wide-web at this URL: http://www.fazend.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@fazend.com so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id$
+ * @category FaZend
+ */
+
+require_once 'FaZend/View/Helper/Forma/Field.php';
+
+/**
+ * Just do nothing
+ *
+ * @package helpers
+ */
+class FaZend_View_Helper_Forma_Behavior_Nothing extends FaZend_View_Helper_Forma_Behavior_Abstract
+{
+
+    /**
+     * Execute it
+     *
+     * @param string HTML to show (form or something else)
+     * @param string Log of the form execution
+     * @return void
+     */
+    public function run(&$html, $log)
+    {
+        // nothing
+    }
+
+}
