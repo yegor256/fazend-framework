29e4e2929aeaa298048b790a04b93f06baf016b4
Yegor Bugayenko <yegor@tpc2.com>
2010-02-09 13:56:54 +0000

    method callback intro
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1616 e43075d4-3bdf-11de-8950-77ccfa13e4cc

---
+++ FaZend/Callback/Method.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * FaZend Framework
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt. It is also available 
+ * through the world-wide-web at this URL: http://www.fazend.com/license
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@fazend.com so we can send you a copy immediately.
+ *
+ * @copyright Copyright (c) FaZend.com
+ * @version $Id: Date.php 1601 2010-02-08 12:10:51Z yegor256@gmail.com $
+ * @category FaZend
+ */
+
+/**
+ * Callback, which uses methods in classes
+ *
+ * @package Callback
+ */
+class FaZend_Callback_Method extends FaZend_Callback
+{
+
+    /**
+     * Calculate and return
+     *
+     * @param array Array of params
+     * @return mixed
+     */
+    protected function _call(array $args)
+    {
+        list($class, $method) = $args;
+        return call_user_func_array(array($class, $method), $args);
+    }
+
+}
