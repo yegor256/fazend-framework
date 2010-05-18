0d2bb26f061e654f4f9b91f42f4456d6a0dc488c
Yegor Bugayenko <yegor@tpc2.com>
2010-05-18 18:53:11 +0000

    shell_exec() removed
    git-svn-id: http://fazend.googlecode.com/svn/trunk@1931 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Revision.php
+++ FaZend/Revision.php
@@ -56,7 +56,8 @@ class FaZend_Revision
             return self::$_revision = file_get_contents($revFile);
         }
         
-        $info = shell_exec('svn info ' . APPLICATION_PATH);
+        $info = FaZend_Exed::exec('svn info ' . APPLICATION_PATH . ' 2>&1');
+        $matches = array();
         if (preg_match('/Revision:\s(\d+)/m', $info, $matches)) {
             return self::$_revision = $matches[1] . 'L';
         }
