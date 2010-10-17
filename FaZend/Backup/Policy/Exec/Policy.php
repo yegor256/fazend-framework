f5a31d3909ec79018e45c5168436d826cc054220
Yegor Bugayenko <yegor@tpc2.com>
2010-10-17 08:51:54 +0000

    nicer validation of policy class
    git-svn-id: http://fazend.googlecode.com/svn/trunk@2217 e43075d4-3bdf-11de-8950-77ccfa13e4cc

--- FaZend/Backup/Policy/Exec/Policy.php
+++ FaZend/Backup/Policy/Exec/Policy.php
@@ -3,7 +3,7 @@
  * FaZend Framework
  *
  * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt. It is also available 
+ * with this package in the file LICENSE.txt. It is also available
  * through the world-wide-web at this URL: http://www.fazend.com/license
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
@@ -34,9 +34,9 @@ class FaZend_Backup_Policy_Exec_Policy extends FaZend_Backup_Policy_Abstract
      */
     protected $_options = array(
         'class' => '?', // PHP class name
-        'options' => array(), // options to send to the policy 
+        'options' => array(), // options to send to the policy
     );
-    
+
     /**
      * Execute another policy.
      *
@@ -45,30 +45,51 @@ class FaZend_Backup_Policy_Exec_Policy extends FaZend_Backup_Policy_Abstract
      * @see FaZend_Backup_Policy_Abstract::forward()
      * @see FaZend_Backup::execute()
      */
-    public function forward() 
+    public function forward()
     {
-        $class = $this->_options['class'];
-        $policy = new $class($this->_log);
-        $policy->setOptions($this->_options['options']);
-        $policy->setDir($this->_dir);
-        logg("Executing {$class}::forward()");
+        $policy = $this->_setupPolicy();
+        logg('Executing %s::forward()', get_class($policy));
         $policy->forward();
     }
-    
+
     /**
      * Execute the policy, in backward direction.
      *
      * @return void
      * @see FaZend_Backup_Policy_Abstract::backward()
      */
-    public function backward() 
+    public function backward()
+    {
+        $policy = $this->_setupPolicy();
+        logg('Executing %s::backward()', get_class($policy));
+        $policy->forward();
+    }
+
+    /**
+     * Setup policy class.
+     *
+     * @return FaZend_Backup_Policy_Abstract
+     * @throws FaZend_Backup_Policy_Exec_Policy_Exception
+     */
+    protected function _setupPolicy()
     {
         $class = $this->_options['class'];
+        if (!class_exists($class)) {
+            FaZend_Exception::raise(
+                'FaZend_Backup_Policy_Exec_Policy_Exception',
+                "Class '{$class}' is absent"
+            );
+        }
         $policy = new $class($this->_log);
+        if (!($policy instanceof FaZend_Backup_Policy_Abstract)) {
+            FaZend_Exception::raise(
+                'FaZend_Backup_Policy_Exec_Policy_Exception',
+                "Class '{$class}' is not inherited from FaZend_Backup_Policy_Abstract"
+            );
+        }
         $policy->setOptions($this->_options['options']);
         $policy->setDir($this->_dir);
-        logg("Executing {$class}::backward()");
-        $policy->backward();
+        return $policy;
     }
-    
+
 }
